a2057808aa5a6acf346267b53d1e7e0f9ccd85c2
Jerome Nika <nika@m2852.ircam.fr>
2018-10-05 14:11:03 +0200

    Revert "ignore"
    This reverts commit 727c9d564feb0f2318c3295e8c5e7d4297200b06.

--- Python_library/DYCI2_Modules/load.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-sys.path.append("/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/")
-sys.path.append("/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/lib-dynload/")
-sys.path.append("/System/Library/Frameworks/Python.framework/Versions/2.7/Extras/lib/python/py2app/recipes/")
-
-from GeneratorBuilder import *
--- Python_library/_MyDev_/Setup_Standalone/build.sh
+++
@@ -1,2 +0,0 @@
-rm -rf build dist
-python setup.py py2app --verbose-interpreter --emulate-shell-environment
