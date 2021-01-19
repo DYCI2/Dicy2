066d0c3b175f4f3be354678a6bd6974cfd152ccd
jnika <jerome.nika@ircam.fr>
2021-01-19 11:38:43 +0100

    fix

--- Python_library/OSCAgent_Tutorial_1.py
+++ Python_library/OSCAgent_Tutorial_1.py
@@ -19,13 +19,13 @@ from DYCI2_Modules.OSCAgentServer import *
 
 # 1) IMPROVISE AND SEND
 
-labels = make_sequence_of_chord_labels(["d m7", "d m7", "g 7", "g 7", "c maj7","c maj7","c# maj7","c# maj7", "d# m7", "d# m7", "g# 7", "g# 7", "c# maj7", "c# maj7"])
-sequence = make_sequence_of_chord_labels(["d m7(1)", "d m7(2)", "g 7(3)", "g 7(4)", "c maj7(5)","c maj7(6)","c# maj7(7)","c# maj7(8)", "d# m7(9)", "d# m7(10)", "g# 7(11)", "g# 7(12)", "c# maj7(13)", "c# maj7(14)"])
+labels = from_list_to_labels(["d m7", "d m7", "g 7", "g 7", "c maj7","c maj7","c# maj7","c# maj7", "d# m7", "d# m7", "g# 7", "g# 7", "c# maj7", "c# maj7"],ChordLabel)
+sequence = from_list_to_labels(["d m7(1)", "d m7(2)", "g 7(3)", "g 7(4)", "c maj7(5)","c maj7(6)","c# maj7(7)","c# maj7(8)", "d# m7(9)", "d# m7(10)", "g# 7(11)", "g# 7(12)", "c# maj7(13)", "c# maj7(14)"],ChordLabel)
 
 print("\nCreation of a OSC agent \nModel type = Factor Oracle\nSequence: {}\nLabels: {}".format(sequence, labels))
 
 authorized_intervals = list(range(-6,6))
-agent = OSCagent(sequence = sequence, labels = labels, model_type = "FactorOracle", authorized_tranformations = authorized_intervals)
+agent = OSCAgent(sequence = sequence, labels = labels, model_type = "FactorOracle", authorized_transformations = authorized_intervals)
 agent.generation_handler.memory.avoid_repetitions_mode = 1 
 agent.generation_handler.memory.max_continuity = 3
 agent.generation_handler.memory.no_empty_event = False
