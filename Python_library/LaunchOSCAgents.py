3b90a47d3128ddcef2a4f0797355479a87b2c38e
jnika <jerome.nika@ircam.fr>
2021-01-25 17:05:33 +0100

    pyinstaller

--- Python_library/LaunchOSCAgents.py
+++ Python_library/LaunchOSCAgents.py
@@ -23,10 +23,11 @@ if __name__ == '__main__':
 	num = 0
 	try:
 		strPAram=sys.argv[1:]
-		#print(strPAram)
+		print(sys.argv)
+		print(strPAram)
 		try:
 			num = int(strPAram[0])
-			#print(num)
+			print(num)
 			if len(strPAram) > 1:
 				print("((Warning: The program should be given only one parameter. The parameter should be the number of Dyci2 agents to launch.))")
 			OSCinport = 4567
@@ -45,6 +46,7 @@ if __name__ == '__main__':
 			print("DYCI2 Agent.\n(The program must be given a parameter. The parameter should be the number of Dyci2 agents to launch.)")
 	except ValueError:
 		print( "Wrong parameter: %s. The parameter should be the number of Dyci2 agents to launch (int)." % num)
+		raise
 
 
 
