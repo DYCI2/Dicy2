0c60956534078c3ae53d31a3a5e066534352d432
jnika <jerome.nika@ircam.fr>
2021-11-15 10:18:40 +0100

    Master OK

--- Externals DYCI2/OK/Packages/MuBu For Max/jsextensions/maxclasswrap.js
+++
@@ -1,5 +0,0 @@
-// this file must be copied in the C74:/jsextensions folder
-// in order to make SimpleJS class accessible
-
-maxclasswrap("mubu.javascript","MubuJS");
-maxclasswrap("mubutrack.javascript","MubuTrackJS");
